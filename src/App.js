3089c049ec9140f5da4998a2da103f82d6c2dea8
Simran Singh <siimransiingh08@gmail.com>
2023-10-26 15:06:23 +0530

    pie chart

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
               <Route path='/line' element={<Line />} />
               <Route path='/area' element={<Area />} />
               <Route path='/bar' element={<Bar />} />
-              <Route path='/piechart' element={<Pie />} />
+              <Route path='/pie' element={<Pie />} />
               <Route path='/financial' element={<Financial />} />
               <Route path='/color-mapping' element={<ColorMapping />} />
               <Route path='/pyramid' element={<Pyramid />} />
